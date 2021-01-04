e437e3617c383efb68d5812d2feea3feaf8fbb6c
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 22:58:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_english_meaning(file, emot)
   hash.each do |mood, langs|
     return mood if langs[:japanese] == emot
   end
-  return "Japanese emoticon not found."
+  return "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon(file, emot)
