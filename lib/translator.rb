39c8fe6c0542cb2a1d39555d4a8c0dd500a2b175
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 22:51:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,13 @@ require 'pry'
 
 def load_library(file)
   hash = YAML.load_file(file)
+  new_hash = {}
+  hash.each do |k, v|
+    new_hash[k] = {
+      english: v[0],
+      japanese: v[1]
+    }
+  end
   binding.pry
   hash
 end
